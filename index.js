2400c519c0bcee9482ebfc91585fa1ab530a6e4d
Damián Adams <contact@mianlabs.com>
2018-07-30 14:25:53 -0400

    Add the implementation of the put method argument interface and return value

--- index.js
+++ index.js
@@ -7,7 +7,25 @@ class Cacherole {
 		this.cache = new cache.Cache();
 	}
 
-	put(options) {}
+	put(options) {
+		// Extract parameters from options object
+		let action, time, binding;
+		if (typeof options === 'function') { 
+			action = options; 
+		} else if (typeof options === 'object' && options !== null) {
+			({ action, time, binding } = options);
+			if (typeof action !== 'function') {
+				throw new TypeError('No valid function argument provided for cacherole put method call');
+			} 
+			if (binding !== undefined) {
+				action = action.bind(binding);
+			}
+		} else {
+			throw new TypeError('Invalid options argument in cacherole put method call');
+		}
+
+		return () => {};
+	}
 }
 
 module.exports = new Cacherole();
